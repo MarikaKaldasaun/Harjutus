f9b68a5642147aeda3cc50468d08a35dc5b50ff9
MarikaKaldasaun <marika.kaldasaun@gmail.com>
2021-01-19 09:47:54 +0200

    Uuendus2

---
+++ src/main/java/ee/bcs/valiit/tasks/Controller/Lesson3Controller.java
@@ -0,0 +1,17 @@
+package ee.bcs.valiit.tasks.Controller;
+
+import ee.bcs.valiit.tasks.solution.SolutionLesson1MathUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ee.bcs.valiit.tasks.solution.SolutionLesson2;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("vastus")
+@RestController
+
+
+public class Lesson3Controller {
+}
