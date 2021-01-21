ebc81835d8eed9e56e74b471fa9506ab0512104e
MarikaKaldasaun <marika.kaldasaun@gmail.com>
2021-01-21 08:33:03 +0200

    Employee

--- src/main/java/ee/bcs/valiit/tasks/EmployeeController.java
+++ src/main/java/ee/bcs/valiit/tasks/EmployeeController.java
@@ -1,20 +1,55 @@
 package ee.bcs.valiit.tasks;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RequestMapping("employee")
 @RestController
 
 public class EmployeeController {
+    List<Test> testList = new ArrayList<>();
+
     @GetMapping( "test")
 //http://localhost:8080/employee/test/?name
             public Test Test(@RequestParam("name") String name) {
                 Test test= new Test();
                 test.setName ( "Kalle");
-                test.setAddress ("Karja");
                 test.setAge (34);
+                test.setId("536");
                 return test;
 
     }
+    @PostMapping ("test")
+    public void test2 (@RequestBody Test test){
+        System.out.println(test.getName());
+    }
+    @PostMapping("lisa")
+    public void addTest (@RequestBody Test newTest){
+        testList.add (newTest);
+    }
+    @GetMapping("nimi")
+    public List<Test> showTest(){
+        return testList;
+    }
+    @GetMapping("isik/{id}")
+    public Test id (@PathVariable ("id")  int idVariable){
+        return testList.get(idVariable);
+    }
+    @PutMapping ("asenda/{id}")
+    public void replaceTest (@RequestBody Test newTest, @PathVariable ("id") int idVariable){
+        testList.set ( idVariable, newTest);
+    }
+    @DeleteMapping ("kustuta/{id}")
+    public void deleteTest (@PathVariable ("id") int idVariable) {
+        testList.remove( idVariable);
+    }
+
+
+
+
 }
--- src/main/java/ee/bcs/valiit/tasks/Test.java
+++ src/main/java/ee/bcs/valiit/tasks/Test.java
@@ -5,6 +5,16 @@ public class Test {
     public String name;
     private String address;
     private int age;
+    private String id;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
